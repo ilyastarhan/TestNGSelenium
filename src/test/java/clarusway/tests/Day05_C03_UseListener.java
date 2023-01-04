9ab8a6fbcc99faf0ca501f4ac7b4167e0bb3f870
ilyastarhan <ilyastarhan.de@gmail.com>
2023-01-04 21:27:07 +0100

    04.01-2

---
+++ src/test/java/clarusway/tests/Day05_C03_UseListener.java
@@ -0,0 +1,33 @@
+package clarusway.tests;
+
+import clarusway.utilities.BaseTestReport;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(Day05_C02_ITestListener.class)
+public class Day05_C03_UseListener {
+
+    @Test
+    public void passedText() {
+        System.out.println("passed Test");
+
+    }
+
+    @Test
+    public void failedText() {
+        System.out.println("failed Test");
+        Assert.fail();
+
+    }
+
+    @Test(timeOut = 500)
+    public void timedOutText() throws InterruptedException {
+        System.out.println("Timed out Test");
+        Thread.sleep(600);
+    }
+
+}
